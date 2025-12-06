2d05ed37a190ae5f239508f8814c6931fabe6830
Rene Hexel <r.hexel@griffith.edu.au>
2025-12-07 09:35:33 +1000

    Factor out ATL (wip).

--- Package.swift
+++ Package.swift
@@ -2,22 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-modelling",
+    name: "swift-atl",
     platforms: [
         .macOS(.v15)
     ],
     products: [
         .library(
-            name: "ECore",
-            targets: ["ECore"]
-        ),
-        .library(
             name: "ATL",
             targets: ["ATL"]
-        ),
-        .executable(
-            name: "swift-ecore",
-            targets: ["swift-ecore"]
         ),
         .executable(
             name: "swift-atl",
@@ -27,36 +19,15 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.6.2"),
         .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.7.0"),
         .package(url: "https://github.com/swiftxml/SwiftXML.git", from: "1.0.0"),
+        .package(url: "https://github.com/mipalgu/swift-ecore.git", branch: "main"),
     ],
     targets: [
         .target(
-            name: "ECore",
-            dependencies: [
-                .product(name: "OrderedCollections", package: "swift-collections"),
-                .product(name: "BigInt", package: "BigInt"),
-                .product(name: "SwiftXML", package: "SwiftXML"),
-            ],
-            swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency")
-            ]
-        ),
-        .target(
             name: "ATL",
             dependencies: [
-                "ECore",
+                .product(name: "ECore", package: "swift-ecore"),
                 .product(name: "OrderedCollections", package: "swift-collections"),
-            ],
-            swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency")
-            ]
-        ),
-        .executableTarget(
-            name: "swift-ecore",
-            dependencies: [
-                "ECore",
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
             swiftSettings: [
                 .enableUpcomingFeature("StrictConcurrency")
@@ -66,7 +37,7 @@ let package = Package(
             name: "swift-atl",
             dependencies: [
                 "ATL",
-                "ECore",
+                .product(name: "ECore", package: "swift-ecore"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
             swiftSettings: [
@@ -74,18 +45,11 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "ECoreTests",
-            dependencies: ["ECore"],
-            resources: [
-                .copy("Resources")
-            ],
-            swiftSettings: [
-                .enableUpcomingFeature("StrictConcurrency")
-            ]
-        ),
-        .testTarget(
             name: "ATLTests",
-            dependencies: ["ATL", "ECore"],
+            dependencies: [
+                "ATL",
+                .product(name: "ECore", package: "swift-ecore"),
+            ],
             resources: [
                 .copy("Resources")
             ],
